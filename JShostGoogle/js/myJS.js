e574a50f9d5556961fea2f3050fbbf32f51b7b6e
Sachithra Dangalla <sachithradangalla@gmail.com>
2016-10-12 13:42:12 +0530

    add tempo

---
+++ JShostGoogle/js/myJS.js
@@ -0,0 +1,5 @@
+/**
+ * Created by sachithra on 10/11/16.
+ */
+d3.select("div").append("p").classed("my-p",true).text("new text new text new text new text new text new text new text ");
+console.log("dsdsds");
