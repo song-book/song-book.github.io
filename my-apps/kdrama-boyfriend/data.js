0148d36143469f455792c4481552df006a3e0af4
song-book <booksongbook@gmail.com>
2016-10-22 14:30:52 +0530

    msg

---
+++ my-apps/kdrama-boyfriend/data.js
@@ -0,0 +1,17 @@
+var data = [
+    {
+        name: "Cha Dae Wong"
+        , drama: "My girlfriend is a Gumiho"
+        , image: "images/dog.png"
+    }
+    , {
+        name: "Cha Dae Wong"
+        , drama: "My girlfriend is a Gumiho"
+        , image: "images/dog.png"
+    }
+    , {
+        name: "Cha Dae Wong"
+        , drama: "My girlfriend is a Gumiho"
+        , image: "images/dog.png"
+    }
+];
